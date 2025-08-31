0ff93fe947d5213135a0b5c849e28f9b618abe76
hamdibillel-maker <billel.hamdi2001@gmail.com>
2025-08-31 16:18:45 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,8 @@
-// Demo program for AS BIT extension
-
-basic.showString("AS BIT")
-
+// Demo: press A to go forward, B to stop
 input.onButtonPressed(Button.A, function () {
-    asbit.move(asbit.Motors.Forward, 80)
+    asbit.move(80, 80)
 })
 
 input.onButtonPressed(Button.B, function () {
-    asbit.move(asbit.Motors.Backward, 80)
-})
-
-input.onButtonPressed(Button.AB, function () {
-    asbit.setRGBDigital(asbit.RGB_Colors.Red)
-    asbit.playTone(440, 500)
-})
-
-basic.forever(function () {
-    if (input.logoIsPressed()) {
-        asbit.move(asbit.Motors.Stop, 0)
-        asbit.setRGBDigital(asbit.RGB_Colors.Off)
-    }
+    asbit.move(0, 0)
 })
