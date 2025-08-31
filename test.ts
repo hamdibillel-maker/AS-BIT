02c4e2ebf8d54f0e6ec6da5f590b00b7b2d184dc
hamdibillel-maker <billel.hamdi2001@gmail.com>
2025-08-31 17:26:13 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,14 @@
-// Demo: A = forward, B = stop
 input.onButtonPressed(Button.A, function () {
-    asbit.move(80, 80)   // both motors forward
+    asbit.move(asbit.MotorDir.Forward, 80)
+    asbit.setRGB(asbit.RGBColors.Green)
 })
+
 input.onButtonPressed(Button.B, function () {
-    asbit.move(0, 0)     // stop
+    asbit.move(asbit.MotorDir.Stop, 0)
+    asbit.setRGB(asbit.RGBColors.Red)
+})
+
+basic.forever(function () {
+    let d = asbit.ultrasonic()
+    basic.showNumber(d)
 })
